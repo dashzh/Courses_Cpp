824e6db6eea20d114aad0d95c75657813fec1b37
dashzh <drammen@ya.ru>
2015-10-18 23:34:15 +0500

    me

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,13 +1,13 @@
-//Список десятичных индексов для функциональных клавиш:
+//Function keys - decimal indices 
 //13 - enter
 //27 - escape
 //8 - backspace
 //32 - space 
 //
-//72 - Стрелка вверх - 22472
-//80 - Стрелка вниз - 22480
-//75 - Стрелка влево - 22475
-//77 - Стрелка вправо - 22477
+//72 - UP - 22472
+//80 - DOWN - 22480
+//75 - LEFT - 22475
+//77 - RIGHT - 22477
 
 #include <iostream>
 #include <windows.h>
@@ -19,8 +19,7 @@
 
 using namespace std;
 
-
-enum unit{Individual,Sprint,Pursuit};
+enum unit{Individual, Sprint, Pursuit};
 
 struct biathlonist
 {
@@ -28,126 +27,98 @@ struct biathlonist
 	string name;
 	int kol_vo_race;
 	double distance;
-//	enum unit{km,ml,oz};
 	unit unit;
 };
-
  
 int help() {
-	cout << "You entered the number of biathlonists and now you cant change it.";
+	cout << "You entered the number of the biathlonists and now you cant change it.";
 	cout << "You can use   arrows like UP or DOWN to  change the menu item.";
 	cout << "To choose the item you should press ENTER and SPACE to back to the main menu! ";
 	return 1;
 	}
 int add(biathlonist *Biathlon, int num) {
-//	struct biathlonist biathlonist_;
-////	getline(cin, biathlonist_.name);
-//	cout << "Name: ";
-//	cin >> biathlonist_.name;
-////	cin.getline(biathlonist_.name, 255);
-//	srand(time(NULL));
-//	biathlonist_.id = rand() %1000 * rand()%10000;
-//	cout << "ID: " << biathlonist_.id << endl;
-//	cout << "kol_vo_race: ";
-//	cin >> biathlonist_.kol_vo_race;
-//	while (biathlonist_.kol_vo_race < 0 ){
-//		cout << "Try again(>0): ";
-//		cin >> biathlonist_.kol_vo_race;
-//	}
-//	cout << "Distance: ";
-//	cin >> biathlonist_.distance;
-//	while (biathlonist_.distance < 0 ){
-//		cout << "Try again(>0): ";
-//		cin >> biathlonist_.distance;
-//	}
-//
-//	Biathlon[num] = biathlonist_;
-//	return 1
-//	int n = num;
-	for (int i = 0; i < num;){
-		if (Biathlon[i].id ==0){
-		cout << i <<"_______________________________________________________"<< endl;
-		cout << "Name: " << Biathlon[i].name;
+	int m;
+	for (int i = 0; i < num;i++){
+		if(Biathlon[i].id == 0) {
+			m = i;
+			break;
+		}
+	}
+	if (m < num){
+		if (Biathlon[m].id ==0){
+		cout << m <<"__________________________________________________________"<< endl;
+		cout << "Name: " << Biathlon[m].name;
 		//getline(cin, Biathlon[i].name);
-		cin >> Biathlon[i].name;
+		cin >> Biathlon[m].name;
 		srand(time(NULL));
-		Biathlon[i].id  = rand() %1000 * rand()%10000;
-		cout << "ID: " << Biathlon[i].id << endl;
-		cout << "Kol_vo of the races: ";
-		cin >> Biathlon[i].kol_vo_race;
-		while (Biathlon[i].kol_vo_race <= 0 ){
-		cout << "Try again(>0): ";
-		cin >> Biathlon[i].kol_vo_race;
-	}
-		cout << "Distance: ";
-		cin >> Biathlon[i].distance;
-		while (Biathlon[i].distance <= 0 ){
-		cout << "Try again(>0): ";
-		cin >> Biathlon[i].distance;
-	}
+		Biathlon[m].id  = rand() %1000 * rand()%10000;
+		cout << "ID: " << Biathlon[m].id << endl;
+			do{
+			cout << "The number of the race(More then NULL): ";
+			cin.clear();
+			cin.sync();
+			cin >> Biathlon[m].kol_vo_race;
+		} while (cin.fail() || ( Biathlon[m].kol_vo_race<0));
+		do{
+			cout << "The number of the race(More then NULL): ";
+			cin.clear();
+			cin.sync();
+			cin >> Biathlon[m].distance;
+			} while (cin.fail() || ( Biathlon[m].distance<0));
 		int units;
 		do{
 			cout << "Enter the units (1-3) 1.Individual, 2.Sprint, 3.Pursuit: ";
 			cin >> units;
 
 		} while (units < 1 || units > 3);
-		Biathlon[1].unit = (unit)units;
-		cin.get();
+		Biathlon[m].unit = (unit)units;
 		cout <<"___________________________________________________________"<< endl;
-		i++;
-		//int press;
-		//cout << "Do you want to continue the entering of the biathlonists? enter BACKSPACE";
-		//press=_getch();
-		//switch (press){
-		//case 8: {
-		//menu(Biathlon, num);
-		//		 break;}
-		//
-		//default:
-		//	break;
-		//}
-		}
-		else {
-		i++;
-		
-		}
+				}
 	}
-//	i++;
+	else {
+		cout << "You cant add new biathlonist. The list is over" << endl;
+	}
 		return 1;
-
 }
+
 int del(biathlonist *Biathlon, int num) {
-	cout << "What biathlonist do you want to delete? Enter the number. " << endl;
-	cout << "This number should be more than 0 and less than " << (num-1) << " : ";
 	int dele;
-	cin >> dele;
-	while ((dele < 0) && (dele > num)){
-		cout << "try again"<< endl;
-		int dele;
+	do {
+		cout << "What biathlonist do you want to delete? Enter the number. " << endl;
+		cout << "This number should be more than 0 and less than " << (num-1) << " : ";
 		cin >> dele;
-	}
+		} while ((dele < 0) || (dele > num));
+		
 		Biathlon[dele].name= " ";
 		Biathlon[dele].id  = 0;
 		Biathlon[dele].kol_vo_race = 0;
 		Biathlon[dele].distance = 0; 
-		/*for (dele; dele<num;dele++){
-		Biathlon[dele].name=Biathlon[dele+1].name;
-		Biathlon[dele].id  = Biathlon[dele+1].id;
-		Biathlon[dele].kol_vo_race = Biathlon[dele+1].kol_vo_race;
-		Biathlon[dele].distance = Biathlon[dele+1].distance; 
-		}*/
-
 	return 1;
 }
 int list(biathlonist *Biathlon, int num) {
 	for (int i = 0; i < num;){
 		if (Biathlon[i].id != 0){
-		cout << i <<"_______________________________________________________"<< endl;
+		cout << i <<"__________________________________________________________"<< endl;
 		cout << "Name: " << Biathlon[i].name << endl;
 		cout << "ID: " << Biathlon[i].id << endl;
-		cout << "Kol_vo " << Biathlon[i].kol_vo_race << endl;
+		cout << "The number of the race: " << Biathlon[i].kol_vo_race << endl;
 		cout << "Distance: " << Biathlon[i].distance << endl;
-		cout << "Units: " << Biathlon[i].unit << endl;
+		//cout << "Units: " << Biathlon[i].unit << endl;
+		cout << "The type of the race: " ;
+		switch (Biathlon[i].unit){
+		case 1:{
+				   cout << "Individual"<< endl;
+				   break;
+		}
+		case 2:{
+				   cout <<" Sprint"<< endl;
+				   break;
+		}
+		case 3:{
+				   cout << "Pursuit"<< endl;
+				   break;
+		}
+		}
 		cout <<"___________________________________________________________"<< endl;
 		i++;
 		}
@@ -190,7 +161,7 @@ int menu(biathlonist *Biathlon, int num){
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD position = {0, 1};
 	cout << "Biathlonists" << endl; 
-	cout << "  1. Help" << endl << "  2. Add" << endl << "  3. Delete" << endl << "  4. All list" << endl << "  5. Exit" << endl; 
+	cout << "* 1. Help" << endl << "  2. Add" << endl << "  3. Delete" << endl << "  4. All list" << endl << "  5. Exit" << endl; 
 	SetConsoleCursorPosition(hConsole, position);
 	
 	while (!exit)
@@ -229,10 +200,10 @@ int menu(biathlonist *Biathlon, int num){
 					}
 					break;
 		}
-		case 13: {
+		case 13: { //enter
 			system("cls");
 			operations(count, Biathlon, num);
-			cout << endl << endl << "To back to the main menu press SPACE ";
+			cout << endl << endl << "Press SPACE to back to the main menu  ";
 			int key = _getch();
 			switch(key){
 			case 32: {
@@ -256,9 +227,18 @@ int menu(biathlonist *Biathlon, int num){
 }
 int main()
 {
-	int num = 4;
+	int num;
+		do{
+			cout << "Enter the  number of the biathlonist. ";
+			cout << "More then NULL: ";
+			cin.clear();
+			cin.sync();
+			cin >> num;
+			//if (cin.fail()) cout << "error" << endl;
+		} while (cin.fail() || (num<0));
+		
+		system("cls");
 	biathlonist *Biathlon = new biathlonist[num];
-	//Biathlon[num].~biathlonist();
 	for (int i = 0; i < num;i++){
 		Biathlon[i].name= " ";
 		Biathlon[i].id  = 0;
